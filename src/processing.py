986b4e743356f88a7dc7b26a61df58d3fe23b284
PC1HA <ilia.popov.2015@gmail.com>
2024-12-20 02:27:50 +0300

    v4.4 fixes

--- src/processing.py
+++ src/processing.py
@@ -2,26 +2,26 @@ from datetime import datetime
 from typing import Any, Dict, List
 
 
-def filter_by_state(list_of_dictionaries: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+def filter_by_state(transactions: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """
     Функция, которая принимает список словарей и значение для ключа "state",
     и возвращает новый список словарей, содержащий только те словари,
     у которых ключ "state" соответствует указанному значению
     """
-    filter_list = []
+    filter_operations = []
 
-    for some_dictionary in list_of_dictionaries:
-        if some_dictionary["state"] == state:
-            filter_list.append(some_dictionary)
+    for transaction in transactions:
+        if transaction["state"] == state:
+            filter_operations.append(transaction)
 
-    return filter_list
+    return filter_operations
 
 
-def sort_by_date(date_list: List[Dict[str, Any]], sort_order: bool = True) -> List[Dict[str, Any]]:
+def sort_by_date(transactions: List[Dict[str, Any]], sort_order: bool = False) -> List[Dict[str, Any]]:
     """
     Функция принимает список словарей и необязательный параметр,
     задающий порядок сортировки (по умолчанию — убывание)
     и возвращает новый список, отсортированный по дате
     """
 
-    return sorted(date_list, key=lambda date_sort: date_sort.get("date", datetime.min), reverse=sort_order)
+    return sorted(transactions, key=lambda transaction: transaction.get("date", datetime.min), reverse=sort_order)
